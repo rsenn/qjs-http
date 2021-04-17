437a7bcc5f3c05c40ac2f65db6b16e814bf66fe7
Roman Senn <roman.l.senn@gmail.com>
2021-04-17 23:22:02 +0200

    ...

--- http-server.js
+++ http-server.js
@@ -1,9 +1,9 @@
-import * as util from "./libhttputil.so";
+import * as util from "libhttputil.so";
 import * as os from "os";
 import * as std from "std";
 import { see } from "./see.js";
 export * from "./see.js";
-export * from "./libhttputil.so";
+export * from "libhttputil.so";
 export { close } from "os";
 
 var _maxRequestSize;
